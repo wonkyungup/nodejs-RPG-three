e75a724a09390f076b740e9cb5bedf7d951e46b4
jjjack <jack@netsarang.com>
2020-07-21 13:45:45 +0900

    sus

--- ws-electron/ws-server/socket.js
+++ ws-electron/ws-server/socket.js
@@ -7,6 +7,7 @@ module.exports = () =>{
         console.log('new Client',ip);
     
         ws.on('message',(message)=>{
+            
             console.log("Client Input: "+ message);
             ws.send(message);
             wsServer.clients.forEach((client)=>{
@@ -14,6 +15,8 @@ module.exports = () =>{
                     client.send(message);
                 }
             })
+            list_save(message);
+            
         });
         ws.on('error',(error)=>{
             console.error(error);
@@ -21,5 +24,23 @@ module.exports = () =>{
         ws.on('close',()=>{
             console.log('close');
     });
-  });        
+  });
+  const list_save = (data) =>{
+    let buffer = [data];
+    console.log(buffer);
+
+    let formatedTimestamp = ()=> {
+        const d = new Date()
+        const date = d.toISOString().split('T')[0];
+        const time = d.toTimeString().split(' ')[0].replace(':','-');
+        return `${date}`
+    }
+
+    const fs = require('fs');
+    fs.appendFile('C:\\Users\\WON\\Desktop\\echo\\ws-electron\\data\\'+formatedTimestamp()+'.txt' , data + "\n" , (err)=>{
+        if(err){
+            throw err;
+        }
+    });
+    };
 };
